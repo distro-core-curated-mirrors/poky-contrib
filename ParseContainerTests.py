41bba3827e73a1b3bc179307bd721ee02f59fe60
bavery <brian.avery@intel.com>
2015-10-16 14:37:01 -0700

    add boot tome collection

--- ParseContainerTests.py
+++ ParseContainerTests.py
@@ -8,6 +8,8 @@ import os
 import sys
 # for searching
 import re
+
+import copy
 '''
 This parses the output of the container based tester. 
 It generates some useful(?) statistics
@@ -26,6 +28,8 @@ def usage():
     Options:
      -d --dir=<directory>                        - defaults to .
      -v --verbose                                - verbose, can specify multiple -v -v -v 
+           1 -v -> more data info
+           2 -v or more -> debugging
      -h --help                                   - prints this
 
     I analyze the container testing results in a given directory.
@@ -55,28 +59,85 @@ class ErrorType(object):
                             print (" I care about this file=%s\n" % (filename))
                         else:
                             print (" I DO NOT care about this file=%s\n" % (filename))
-                    
 
-
-def getStartupTimes(rootDir,dir):
+# This allows me to extract the kernel boot time. on reflection
+# I think the total time is fine to start with. We can use this 
+# as a guide if I want to break it up later.
+# unused!
+def getKernelBootTime(rootDir,dir):
     testDir=rootDir+"/"+dir+"/"+"testimage"
-    print "getStartupTimes:testDir=%s"%testDir
     patternKernel=re.compile(".*Startup finished in (\d+)\.(\d+)s \(kernel\).*")
-    #patternKernel=re.compile(".*Startup finished in.*")
     for dirName,subdirList,fileList in os.walk(testDir):
         for f in fileList:
             if "ssh_target_log." in f:
-                print "getStartupTimes: f=%s"%f
                 filename=testDir+"/"+f
                 f=open(filename,'r')
                 txt = f.read()
                 f.close()
                 mKernel = patternKernel.search(txt)
                 if mKernel:
-                    print "mKernel num groups=%d"%len(mKernel.groups())
-                    print "group0 = ",mKernel.group(0)
-                    for g in mKernel.groups():
-                        print "mKernalall = ",g
+                    if len(mKernel.groups()) == 2:
+                        kernelBooTime = (float)(mKernel.group(1)) + (float)(mKernel.group(2))/1000.0
+    try:
+        return kernelBootTime
+    except NameError:
+        return 0.0
+
+
+
+
+# returns the sum of kernel and user boot time
+def getTotalBootTime(rootDir,dir):
+    testDir=rootDir+"/"+dir+"/"+"testimage"
+    totalBootTime=0.0
+    if gVerbose >=3:
+        print "getTotalBoot:testDir=%s"%testDir
+
+    patternBootMin=re.compile(".*Startup finished in.*\(kernel\).*\= (\d+)min (\d+)\.(\d+)s.*")
+    patternBootSec=re.compile(".*Startup finished in.*\(kernel\).*\= (\d+)\.(\d+)s.*")
+    #patternKernel=re.compile(".*Startup finished in (\d+)\.(\d+)s \(kernel\).*")
+
+    for dirName,subdirList,fileList in os.walk(testDir):
+        for f in fileList:
+            if "ssh_target_log." in f:
+                #print "getStartupTimes: f=%s"%f
+                filename=testDir+"/"+f
+                f=open(filename,'r')
+                txt = f.read()
+                f.close()
+                mBootMin = patternBootMin.search(txt)
+                if mBootMin:
+                    if gVerbose >=3:
+                        print "mBootMin num groups=%d"%len(mBootMin.groups())
+                        print "group0 = ",mBootMin.group(0)
+                    if len(mBootMin.groups()) == 3:
+                        totalBootTime = (float)(mBootMin.group(1))*60.0+(float)(mBootMin.group(2)) + (float)(mBootMin.group(3))/1000.0
+                        if gVerbose >=3:
+                            print "total time as float = %f" % totalBootTime
+                else:
+                    # bootmin failed. either we booted in < 1 min or didn't boot at all
+                    mBootSec = patternBootSec.search(txt)
+                    if mBootSec:
+                        if gVerbose >=3:
+                            print "mBootSec num groups=%d"%len(mBootSec.groups())
+                            print "group0 = ",mBootSec.group(0)
+                        if len(mBootSec.groups()) == 2:
+                            totalBootTime = (float)(mBootSec.group(1)) + (float)(mBootSec.group(2))/1000.0
+                            if gVerbose >=3:
+                                print "total time as float = %f" % totalBootTime
+                    
+    #try:
+    return totalBootTime
+    #except NameError:
+    #    return 0.0
+    
+        
+
+def getStartupTime(rootDir,dir):
+    #    bootTime = getTotalBootTime(rootDir,dir)
+    bootTime = getTotalBootTime(rootDir,dir)
+    return bootTime
+    
 
 def isTestDir(rootDir,dir):
     return True if "testrun" in dir else False
@@ -91,14 +152,14 @@ def isSimpleRegex(rootDir,dir,regex=".*"):
 
     if "-failure" in dir:
         filename = rootDir+"/"+dir+"/stdout"
-        if gVerbose:
+        if gVerbose>=3:
             print "isSimpleRegex:%s"%filename
         f=open(filename,'r')
         txt = f.read()
         f.close()
         m = pattern.search(txt)
         if m:
-            if gVerbose:
+            if gVerbose>=3:
                 print "Group0 = [%s]"%m.group(0)
                 print "m=",m.group(0)
             return True
@@ -118,7 +179,7 @@ def isBlutoothWpaAvahiTimeout(rootDir,dir):
 
     if "-failure" in dir:
         filename = rootDir+"/"+dir+"/stdout"
-        if gVerbose:
+        if gVerbose>=3:
             print "isBlutoothWpaAvahiTimeout:%s"%filename
         f=open(filename,'r')
         txt = f.read()
@@ -127,12 +188,10 @@ def isBlutoothWpaAvahiTimeout(rootDir,dir):
         m2 = pattern2.search(txt)
         m3 = pattern3.search(txt)
         if m1 and m2 and m3:
-            if gVerbose:
+            if gVerbose>=3:
                 print "M1Group0 = [%s]"%m1.group(0)
                 print "M2Group0 = [%s]"%m2.group(0)
                 print "M3Group0 = [%s]"%m3.group(0)
-
-            getStartupTimes(rootDir,dir)
             return True
         else:
             return False
@@ -153,16 +212,17 @@ def HandleTests(tests,rootDir):
                     if t['test'](rootDir,sub):
                         t['count']+=1
                         t['dirlist'].append(sub)
+                        if t.has_key('startupTime'):
+                            t['startupTime'][sub]=getStartupTime(rootDir,sub)
         break
     return 
 
 
 def PrintStats(tests):
-
-
     for t in tests:
         if "Total" in t['name']:
             totalRuns=t['count']
+            fullTimeList = t['startupTime']
         if "Failures" in t['name']:
             totalFailures=t['count']
             uncharacterizedDirs=t['dirlist']
@@ -173,6 +233,31 @@ def PrintStats(tests):
         if "Failures" in t['name']:
             continue
         print "test %s has a count of %d for a percentage of %02.2f%%" % (t['name'],t['count'],per)
+
+        # get some time info
+        timeList = []
+        for d in t['dirlist']:
+            timeList.append(fullTimeList[d])
+        # 0.0 means we don't know anything so don't mix it into the stats
+        timeListStat=[x for x in timeList if x > 0]
+        try:
+            timeListStat.remove(0.0)
+        except ValueError:
+            pass
+        if len(timeListStat):
+            print ("\tStartup Time Information (0.0 sec entries removed):\n")
+            print("\t\t Min Startup Time: %f" % min(timeListStat))
+            print("\t\t Max Startup Time: %f" % max(timeListStat))
+            print("\t\t Avg Startup Time: %f" % (sum(timeListStat)/float(len(timeListStat))))
+            if gVerbose:
+                print("\t In case you want to dive deeper,here are the dir:times -- ")
+                for time,dir in zip(timeList,t['dirlist']):
+                    print "\t\tdir:%s took %f sec"%(dir,time)
+        else:
+            print "\tNo Time information for this test :("
+
+
+        # figure out how many failures do not have matching tests yet
         totalFailures-=t['count']
         u=[x for x in uncharacterizedDirs if x not in t['dirlist']]
         uncharacterizedDirs=u
@@ -244,7 +329,7 @@ if __name__ == "__main__":
           },             
 
 
-
+             # summation tests
              {"name":"Failures",
              "test":isFailDir,
               "dirlist":[],
@@ -253,6 +338,9 @@ if __name__ == "__main__":
              {"name":"Total",
              "test":isTestDir,
               "dirlist":[],
+              # this is a dict of startuptimes by testdir so that we can look it up
+              # it's too expensive to run this more than once.
+              "startupTime":{},
               "count":0
           }
          ]
