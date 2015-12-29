b936f456fa348a409619071c01e255638158d10b
jwang <jing.j.wang@intel.com>
2015-12-29 15:17:45 +0800

    QA: testrunner: Dump stdout/stderro to external log file
    Dump stdout and stderr during testing to an output file. This commit implement this function by
    replace sys stdout/stderr with a customized stream object which support the extern file.
    
    Signed-off-by: jwang <jing.j.wang@intel.com>

--- meta/lib/oeqa/base/baserunner.py
+++ meta/lib/oeqa/base/baserunner.py
@@ -12,7 +12,9 @@
 import os
 import sys
 import unittest
+import shutil
 from optparse import OptionParser, make_option
+from util.log import LogHandler
 
 class TestContext(object):
     '''test context which inject into testcase'''
@@ -27,6 +29,7 @@ class TestRunnerBase(object):
         self.runner = None
         self.context = None
         self.manifest = None
+        self.log_handler = None
 
     @staticmethod
     def __get_tc_from_manifest(fname):
@@ -36,6 +39,24 @@ class TestRunnerBase(object):
                                 if n.strip() and not n.strip().startswith('#')]
         return tclist
 
+    def _get_log_dir(self, logdir=None):
+        '''get the log directory'''
+        if not logdir:
+            logdir = os.path.abspath(os.path.join(".", "log"))
+            if not os.path.exists(logdir):
+                os.makedirs(logdir)
+            prefix = self.manifest[:self.manifest.rindex(".")]+"%03d"
+            names = set(os.listdir(logdir))
+            index = len(names)
+            while prefix % index not in names and index >= 1:
+                index -= 1
+            logdir = os.path.join(logdir, prefix % (index + 1))
+        else:
+            if os.path.exists(logdir):
+                shutil.rmtree(logdir)
+        os.makedirs(logdir)
+        return logdir
+
     def options(self):
         '''handle testrunner options'''
         self.option_list = [
@@ -43,6 +64,8 @@ class TestRunnerBase(object):
                     help="The test list file"),
             make_option("-x", "--xunit", dest="xunit",
                     help="Output result path of in xUnit XML format"),
+            make_option("-l", "--log-dir", dest="logdir",
+                    help="Set log dir."),
         ]
 
     def configure(self, options):
@@ -67,6 +90,10 @@ class TestRunnerBase(object):
             self.tclist = self.__get_tc_from_manifest(options.manifest)
         else:
             raise Exception("please specify a manifest file by -f")
+
+        if options.logdir:
+            logdir = self._get_log_dir(options.logdir)
+            self.log_handler = LogHandler(logdir)
 
         self.context = TestContext()
 
@@ -93,8 +120,12 @@ class TestRunnerBase(object):
         options = parser.parse_args()[0]
         self.configure(options)
         print options
+        if self.log_handler:
+            self.log_handler.start()
         self.start()
         self.result()
+        if self.log_handler:
+            self.log_handler.end()
 
 if __name__ == "__main__":
     TestRunnerBase().run()
