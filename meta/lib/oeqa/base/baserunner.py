bc026617ec5208737144cc187a624a41d0a8bd74
zjh <junhuix.zhang@intel.com>
2016-01-21 15:20:27 +0800

    Meta: oeqa: Extract common bits of loadTests and runTests from oetest.py to base test runner
    loadTests and runTests are most fundamental functions of a test runner. This
    commit extract common bits of them from oetest.py to form the skeleton of base
    test runner - class TestRunnerBase under oeqa/base/baserunner.py, and migrate
    original loadTests and runTests functions to base test runner accordingly.
    
    Signed-off-by: zjh <junhuix.zhang@intel.com>

---
+++ meta/lib/oeqa/base/baserunner.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# Copyright (C) 2013 Intel Corporation
+#
+# Released under the MIT license (see COPYING.MIT)
+
+# Base unittest module used by testrunner
+# This provides the common test runner functionalities including manifest input,
+# xunit output, timeout, tag filtering.
+
+"""Base testrunner"""
+
+import os
+import sys
+import time
+import unittest
+
+
+class TestContext(object):
+    '''test context which inject into testcase'''
+    def __init__(self):
+        self.target = None
+        
+
+class TestRunnerBase(object):
+    '''test runner base '''
+    def __init__(self, context=None):
+        self.tclist = []
+        self.runner = None
+        self.context = context if context else TestContext()
+        self.test_result = None
+
+    def configure(self, options=None):
+        '''configure before testing'''
+        self.runner = unittest.TextTestRunner(stream=sys.stderr, verbosity=2)
+        self.tclist = self.tclist if self.tclist else getattr(self.context, "testslist", [])
+        setattr(unittest.TestCase, "tc", self.context)
+
+    def loadtest(self, names=None):
+        '''load test suite'''
+        if not names:
+            names = self.tclist
+        testloader = unittest.TestLoader()
+        tclist = []
+        for name in names:
+            tset = testloader.loadTestsFromName(name)
+            if tset.countTestCases() > 0:
+                tclist.append(tset)
+            elif tset._tests == []:
+                tclist.append(testloader.discover(name, "[!_]*.py"))
+        return testloader.suiteClass(tclist)
+
+    def runtest(self, testsuite):
+        '''run test suite'''
+        self.test_result = self.runner.run(testsuite)
+
+    def start(self, testsuite):
+        '''start testing'''
+        setattr(unittest.TestCase, "tc", self.context)
+        self.runtest(testsuite)
