7ce4b3c2d46ad0ae6eb3cf736219ee212c2b71da
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-01-26 08:41:42 +0000

    tests/test_repository: Repository test suite
    Basic unit tests that test the repo.repodir value: directory existance,
    git repository and git config file.
    
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

---
+++ tests/test_repository.py
@@ -0,0 +1,19 @@
+import os
+import unittest
+from patchtestargs import PatchTestArgs as pta
+
+class TestRepository(unittest.TestCase):
+    def test_repo_existance(self):
+        """ Test existance of the repository folder """
+        self.assertTrue(os.path.isdir(pta.repodir), 'The repository does not exist')
+
+    def test_git_repo(self):
+        """ Test existance of of the .git folder """
+        git_folder = os.path.join(pta.repodir, '.git')
+        self.assertTrue(os.path.isdir(git_folder), 'The repository (%s) is not a git repo' % git_folder)
+
+    def test_git_config(self):
+        """ Test existance of the git configuration file """
+        git_config = os.path.join(pta.repodir, '.git', 'config')
+        self.assertTrue(os.path.isfile(git_config), 'The repository does not contain patchwork configuration data')
+
