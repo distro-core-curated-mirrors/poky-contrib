08688a70272d5bba6b026c83574a4be5b898bc3b
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-02-08 09:22:49 +0000

    test_sample: remove assertion that fails
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -4,7 +4,4 @@ class TestSample(unittest.TestCase):
     def test_pass(self):
         """ Sample test, should pass """
         self.assertTrue(True, "test sample, always pass")
-    def test_fail(self):
-        """ Sample test, should fail """
-        self.assertTrue(False, "test sample, always fails")
 
