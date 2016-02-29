84ea66ea7cf002f9d5f9feca30d0ff5a02a04ea0
Belen Barros Pena <belen.barros.pena@linux.intel.com>
2016-02-29 15:46:58 +0000

    A design proposal for the Git Notes application
    Signed-off-by: Belen Barros Pena <belen.barros.pena@linux.intel.com>

---
+++ js/ajax.js
@@ -0,0 +1,18 @@
+/* git-notes-web - ajax helper code
+ * Based on examples from the CSRF page in the Django manual
+ */
+
+var csrftoken = $.cookie('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    crossDomain: false, // obviates need for sameOrigin test
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type)) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
