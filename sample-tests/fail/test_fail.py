15e316c23f93bec56cbd9ac70bf3a668bdf7a69d
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-03-07 09:15:46 +0000

    sample-tests: Remove sample test cases
    Sample tests should be added on [1], a sample test suite to be used with
    patchtest.
    
    [1] https://github.com/lsandoval/patchtest-sample-test-suite
    
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- sample-tests/fail/test_fail.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-class TestFail(unittest.TestCase):
-    def test_fail(self):
-        """ Sample test, should fail """
-        self.assertTrue(False, "test sample, always fail")
--- sample-tests/success/test_sample.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-class TestSample(unittest.TestCase):
-    def test_pass(self):
-        """ Sample test, should pass """
-        self.assertTrue(True, "test sample, always pass")
-
