6a7b9074dddf45598818b5e62ec5227a4f3cc430
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-03-08 13:36:25 +0000

    patchtest: lib/summary: new summary report using jinja2 module
    New summary class using the jinja2 module. Currently hardcoded as the
    default one.
    
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- lib/summary.py
+++ lib/summary.py
@@ -1,13 +1,17 @@
 import os
 import utils
 from tabulate import tabulate
+from jinja2 import Template
 
 class BaseSummary(object):
     FAIL = 'FAIL'
     SUCCESS = 'PASS'
     SKIP = 'SKIP'
 
-    def __init__(self):
+    def __init__(self, project='', mailinglist=''):
+        self._project = project
+        self._mailinglist = mailinglist
+
         self._results = list()
 
     def addFailure(self, test):
@@ -27,9 +31,6 @@ class TabulateSummary(BaseSummary):
     result_header = ['Result', 'Test Description']
     tablefmt = 'simple'
 
-    def __init__(self):
-        self._results = list()
-
     def generate(self, items):
         text = str()
         merge_results = [[item.status, item.resource] for item in items]
@@ -41,3 +42,33 @@ class TabulateSummary(BaseSummary):
             text += "\n\n%s\n" % tabulate(test_results, self.result_header, tablefmt=self.tablefmt)
 
         return text
+
+
+class TemplateSummary(BaseSummary):
+
+    # TODO: the template should be located in a separate file
+    template = """
+Thanks for your patch submission {% if resource %} of {{ resource }} {% endif %}to {{ project }} - this is an automated response. The results of some tests on the patch(es) are as follows:
+
+{% for result, description in testresults %}
+    {{ result }} {{ description }}
+{% endfor %}
+
+If you believe any of these test results are incorrect, please reply to the mailing list ({{ mailinglist }}) raising your concerns. Otherwise we
+would appreciate you correcting the issues and submitting a new version of the patchset if applicable. Please ensure you add/increment the version number
+when sending the new version (i.e. [PATCH] -> [PATCH v2] -> [PATCH v3] -> ...).
+"""
+
+    def generate(self, items):
+        resource = None
+        testresults = [(result, test.shortDescription()) for result, test in self._results]
+        try:
+            item = items[0]
+            resource = item.resource
+        except:
+            pass
+
+        return Template(self.template).render(project=self._project,
+                                              mailinglist=self._mailinglist,
+                                              resource=resource,
+                                              testresults=testresults)
