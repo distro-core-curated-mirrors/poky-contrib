8e715cb571f4508ae7cea9580e0d2960d105bbcf
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-03-08 07:46:16 +0000

    summary: Create a summary base class and refactor the tabulate class
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- lib/summary.py
+++ lib/summary.py
@@ -2,32 +2,42 @@ import os
 import utils
 from tabulate import tabulate
 
-class BasicSummary(dict):
+class BaseSummary(object):
+    FAIL = 'FAIL'
+    SUCCESS = 'PASS'
+    SKIP = 'SKIP'
+
+    def __init__(self):
+        self._results = list()
+
+    def addFailure(self, test):
+        self._results.append((self.FAIL, test))
+
+    def addSuccess(self, test):
+        self._results.append((self.SUCCESS, test))
+
+    def addSkip(self, test):
+        self._results.append((self.SKIP, test))
+
+    def generate(self, items):
+        raise Exception
+
+class TabulateSummary(BaseSummary):
     merge_header = ['Merge Status', 'Item']
-    result_header = ['Result', 'Test Description', 'Assertion Description']
+    result_header = ['Result', 'Test Description']
     tablefmt = 'simple'
 
     def __init__(self):
         self._results = list()
 
-    def addFailure(self, test, err):
-        _, assertionDescription, _ = err
-        self._results.append(['FAIL: ', test.shortDescription()])
+    def generate(self, items):
+        text = str()
+        merge_results = [[item.status, item.resource] for item in items]
+        if merge_results:
+            text += "\n\n%s\n" % tabulate(merge_results, self.merge_header, tablefmt=self.tablefmt)
 
-    def addSuccess(self, test):
-        self._results.append(['PASS: ', test.shortDescription()])
-
-    def addSkip(self, test, msg):
-        self._results.append(['SKIP: ', test.shortDescription()])
-
-    def generateSummary(self, items):
-        """ Generate and store the summary """
-        text = ''
-        if items:
-            merge_results = [[item.status, item.resource] for item in items]
-            text += "\n\n%s\n" % tabulate(merge_results, BasicSummary.merge_header, tablefmt=BasicSummary.tablefmt)
-
-        if self._results:
-            text += "\n\n%s\n" % tabulate(self._results, BasicSummary.result_header, tablefmt=BasicSummary.tablefmt)
+        test_results = [(result, test.shortDescription()) for result, test in self._results]
+        if test_results:
+            text += "\n\n%s\n" % tabulate(test_results, self.result_header, tablefmt=self.tablefmt)
 
         return text
