0b9980190f01f26b955c0ec4be6df7e453a09acd
Ed Bartosh <ed.bartosh@linux.intel.com>
2016-03-17 10:49:21 +0200

    toaster: fix conflicting migrations
    Fixed by running manage.py makemigrations --merge
    
    Signed-off-by: Ed Bartosh <ed.bartosh@linux.intel.com>

---
+++ bitbake/lib/toaster/bldcontrol/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bldcontrol', '0002_add_cancelling_state'),
+        ('bldcontrol', '0002_auto_20160120_1250'),
+    ]
+
+    operations = [
+    ]
---
+++ bitbake/lib/toaster/orm/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('orm', '0006_add_cancelled_state'),
+        ('orm', '0006_remove_build_build_name'),
+    ]
+
+    operations = [
+    ]
