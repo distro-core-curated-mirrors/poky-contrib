9972292f82ad49eb36bdc6ef640177ae16d073d6
Belen Barros Pena <belen.barros.pena@linux.intel.com>
2016-04-15 12:46:03 +0100

    toaster: Fix filters
    Set the correct size for the filter buttons, and give some white space
    to the 'clear filter' buttons in the toastertables.
    
    Signed-off-by: Belen Barros Pena <belen.barros.pena@linux.intel.com>

--- bitbake/lib/toaster/toastergui/static/js/table.js
+++ bitbake/lib/toaster/toastergui/static/js/table.js
@@ -232,7 +232,7 @@ function tableInit(ctx){
 
       /* Setup the filter button */
       if (col.filter_name){
-        var filterBtn = $('<a href="#" role="button" data-filter-on="' + col.filter_name + '" class="pull-right btn btn-xs" data-toggle="modal"><i class="glyphicon glyphicon-filter filtered"></i></a>');
+        var filterBtn = $('<a href="#" role="button" data-filter-on="' + col.filter_name + '" class="pull-right btn btn-mini" data-toggle="modal"><i class="glyphicon glyphicon-filter filtered"></i></a>');
 
         filterBtn.data('filter-name', col.filter_name);
         filterBtn.prop('id', col.filter_name);
@@ -282,7 +282,7 @@ function tableInit(ctx){
 
       filterBtn.tooltip({
           html: true,
-          title: '<button class="btn btn-sm btn-primary" onClick=\'$("#clear-filter-btn-'+ ctx.tableName +'").click();\'>Clear filter</button>',
+          title: '<button class="btn btn-small btn-primary" onClick=\'$("#clear-filter-btn-'+ ctx.tableName +'").click();\'>Clear filter</button>',
           placement: 'bottom',
           delay: {
             hide: 1500,
