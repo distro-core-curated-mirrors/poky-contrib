03d41a6a9035fe8cc7cf7483c2e9bf59237186b7
Richard Purdie <richard.purdie@linuxfoundation.org>
2016-05-20 12:40:09 +0100

    Revert "siggen: Experiment to always write data about the base siginfo"
    This reverts commit f5c3b56e5e2cc32380895ca476ca7dbbdf86e81c.

--- bitbake/lib/bb/siggen.py
+++ bitbake/lib/bb/siggen.py
@@ -140,9 +140,6 @@ class SignatureGeneratorBasic(SignatureGenerator):
         self.gendeps[fn] = gendeps
         self.lookupcache[fn] = lookupcache
 
-        for task in tasklist:
-            self.dump_sigtask(fn, task, d.getVar("STAMP", True), False)
-
         return taskdeps
 
     def finalise(self, fn, d, variant):
