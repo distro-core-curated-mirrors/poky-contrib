faf904c62336d67943f0ec0024ac5e20f53dd7c7
Elliot Smith <elliot.smith@intel.com>
2016-07-05 11:19:30 +0100

    toaster: add package manifest path to Target objects
    Store the path to the *.rootfs.manifest file for targets which
    generate images.
    
    A link to the package manifest is displayed in the build dashboard
    for targets which produce image files.
    
    Like the license manifest path, if a target would have produced
    the package manifest (but didn't, because it already existed), that
    path is copied from the target which did produce the package
    manifest.
    
    Signed-off-by: Elliot Smith <elliot.smith@intel.com>
    Signed-off-by: bavery <brian.avery@intel.com>

--- bitbake/lib/toaster/orm/models.py
+++ bitbake/lib/toaster/orm/models.py
@@ -730,9 +730,9 @@ class Target(models.Model):
         Target_Image_File object for an ext4 image being associated with a
         target for a project which didn't produce an ext4 image (for example).
 
-        Also sets the license_manifest_path of this target to the same path
-        as that of target being cloned from, as the license manifest path is
-        also a build artifact but is treated differently.
+        Also sets the license_manifest_path and package_manifest_path
+        of this target to the same path as that of target being cloned from, as
+        the manifests are also build artifacts but are treated differently.
         """
 
         image_fstypes = self.build.get_image_fstypes()
@@ -755,6 +755,7 @@ class Target(models.Model):
             kernel_file.save()
 
         self.license_manifest_path = target.license_manifest_path
+        self.package_manifest_path = target.package_manifest_path
         self.save()
 
     def clone_sdk_artifacts_from(self, target):
