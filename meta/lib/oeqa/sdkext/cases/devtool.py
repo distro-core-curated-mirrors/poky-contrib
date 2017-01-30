728eb8a229864afa08035e462105cf2288e04e63
Francisco Pedraza <francisco.j.pedraza.gonzalez@intel.com>
2017-01-30 13:39:40 -0600

    sdkext/cases oeqa/selftest: Updates decorators
    Updates decorators for eSDK in test cases at:
    sdkext/cases/devtool.py and oeqa/selftest/eSDK.py
    
    Signed-off-by: Francisco Pedraza <francisco.j.pedraza.gonzalez@intel.com>

--- meta/lib/oeqa/sdkext/cases/devtool.py
+++ meta/lib/oeqa/sdkext/cases/devtool.py
@@ -49,22 +49,22 @@ class DevtoolTest(OESDKExtTestCase):
         self._run('devtool add myapp %s' % self.myapp_dst)
         self._run('devtool reset myapp')
     
-    @OETestID(1473)
+    @OETestID(1605)
     @OETestDepends(['test_devtool_location'])
     def test_devtool_build_make(self):
         self._test_devtool_build(self.myapp_dst)
     
-    @OETestID(1474)
+    @OETestID(1606)
     @OETestDepends(['test_devtool_location'])
     def test_devtool_build_esdk_package(self):
         self._test_devtool_build_package(self.myapp_dst)
 
-    @OETestID(1479)
+    @OETestID(1607)
     @OETestDepends(['test_devtool_location'])
     def test_devtool_build_cmake(self):
         self._test_devtool_build(self.myapp_cmake_dst)
     
-    @OETestID(1482)
+    @OETestID(1608)
     @OETestDepends(['test_devtool_location'])
     def test_extend_autotools_recipe_creation(self):
         req = 'https://github.com/rdfa/librdfa'
@@ -78,7 +78,7 @@ class DevtoolTest(OESDKExtTestCase):
             raise e
         self._run('devtool reset %s' % recipe)
 
-    @OETestID(1484)
+    @OETestID(1609)
     @OETestDepends(['test_devtool_location'])
     def test_devtool_kernelmodule(self):
         docfile = 'https://github.com/umlaeute/v4l2loopback.git'
@@ -92,7 +92,7 @@ class DevtoolTest(OESDKExtTestCase):
             raise e
         self._run('devtool reset %s' % recipe)
 
-    @OETestID(1478)
+    @OETestID(1610)
     @OETestDepends(['test_devtool_location'])
     def test_recipes_for_nodejs(self):
         package_nodejs = "npm://registry.npmjs.org;name=winston;version=2.2.0"
