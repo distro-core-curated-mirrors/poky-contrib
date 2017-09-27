8ff7c517a996132b8f5ad5a7bc1fdf95e312c2c5
libertad <libertad.cruz@intel.com>
2017-09-27 11:52:12 -0700

    dpky.py: Adds testcases for dpkg package manager
    Adds dpkg testcases for testing basic operation functionality for the dpkg packgage manager.Adds new assert function to verify expected results given by the dpkg package.
    
    [YOCTO #10063]
    
    Signed-off-by: libertad <libertad.cruz@intel.com>

--- meta/lib/oeqa/runtime/cases/dpkg.py
+++ meta/lib/oeqa/runtime/cases/dpkg.py
@@ -10,9 +10,10 @@ from oeqa.runtime.decorator.package import OEHasPackage
 
 
 class dpkgTest(OERuntimeTestCase):
-    def dpkg(self, command, expected=0):
+    def dpkg(self, command, expectedStr, expected=0):
         status, output = self.target.run(command, 1500)
         message = os.linesep.join([command, output])
+        self.assertIn(expectedStr, output) 
         self.assertEqual(status, expected, message)
         return output
 
@@ -24,29 +25,29 @@ class dpkgBasicTest(dpkgTest):
     @OETestDepends(['ssh.SSHTest.test_ssh'])
     @OETestID(1801)
     def test_dpkg_help(self):
-        self.dpkg('dpkg --help')
+        self.dpkg('dpkg --help',"Usage: dpkg [<option> ...] <command>")
 
     @OETestDepends(['dpkg.dpkgBasicTest.test_dpkg_help'])
     @OETestID(1802)
     def test_dpkg_version(self):
-        self.dpkg('dpkg --version')
+        self.dpkg('dpkg --version',"Debian 'dpkg' package management program version")
 
     @OETestID(1803)
     def test_dpkg_Dhelp(self):
-        self.dpkg('dpkg -Dhelp')
+        self.dpkg('dpkg -Dhelp',"dpkg debugging option, --debug=<octal> or -D<octal>:")
 
     @OETestID(1805)
     def test_dpkg_force_help(self):
-        self.dpkg('dpkg --force-help')
+        self.dpkg('dpkg --force-help',"dpkg forcing options - control behaviour when problems found:")
 
     @OETestID(1806)
     def test_dpkg_deb_help(self):
-        self.dpkg('dpkg-deb --help')
+        self.dpkg('dpkg-deb --help',"Usage: dpkg-deb [<option> ...] <command>")
 
     @OETestID(1812)
     def test_dpkg_status(self):
-        self.dpkg('dpkg -s dpkg')
+        self.dpkg('dpkg -s dpkg',"Package: dpkg")
 
     @OETestID(1814)
     def test_dpkg_list(self):
-        self.dpkg('dpkg -l')
+        self.dpkg('dpkg -l',"Desired=Unknown/Install/Remove/Purge/Hold")
