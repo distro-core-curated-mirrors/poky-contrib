027e3c46bf66d7bf00e81fe2fe6f36917b1b312c
Paul Eggleton <paul.eggleton@linux.intel.com>
2017-12-04 10:42:13 +1300

    devtool: fix handling of linux-yocto after multisrc changes
    If the kernel metadata repository has been set up by devtool
    modify along with the source, we need to ensure it isn't overwritten
    and it can be found by the kernel git tools.
    
    Signed-off-by: Paul Eggleton <paul.eggleton@linux.intel.com>

--- scripts/lib/devtool/standard.py
+++ scripts/lib/devtool/standard.py
@@ -909,7 +909,9 @@ def modify(args, config, basepath, workspace):
 
             if bb.data.inherits_class('kernel', rd):
                 f.write('SRCTREECOVEREDTASKS = "do_validate_branches do_kernel_checkout '
-                        'do_fetch do_unpack do_kernel_configme do_kernel_configcheck"\n')
+                        'do_fetch do_kernel_configme do_kernel_configcheck"\n')
+                if bb.data.inherits_class('kernel-yocto', rd):
+                    f.write('EXTERNALSRC_KMETA = "1"\n')
                 f.write('\ndo_patch() {\n'
                         '    :\n'
                         '}\n')
@@ -917,6 +919,8 @@ def modify(args, config, basepath, workspace):
                         '    cp ${B}/.config ${S}/.config.baseline\n'
                         '    ln -sfT ${B}/.config ${S}/.config.new\n'
                         '}\n')
+                f.write('\ndef find_kernel_feature_dirs(d):\n'
+                        '    return [d.getVar("KMETA")]\n\n')
 
             if extradirs:
                 f.write('\n# srctreetop: %s\n' % srctreetop)
