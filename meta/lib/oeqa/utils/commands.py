560909f4663084286211134e005076542ce675df
Ross Burton <ross.burton@intel.com>
2018-03-14 17:26:35 +0000

    XX fixup get_bb_vars

--- meta/lib/oeqa/utils/commands.py
+++ meta/lib/oeqa/utils/commands.py
@@ -239,7 +239,7 @@ def get_bb_vars(variables=None, target=None, postconfig=None):
     if variables is not None:
         variables = list(variables)
     var_re = re.compile(r'^(export )?(?P<var>\w+(_.*)?)="(?P<value>.*)"$')
-    unset_re = re.compile(r'^unset (?P<var>\w+)$')
+    unset_re = re.compile(r'^unset (?P<var>\w+)')
     lastline = None
     values = {}
     for line in bbenv.splitlines():
