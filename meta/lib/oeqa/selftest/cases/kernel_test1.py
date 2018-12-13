ae6613166077c6fd72c4847ee0c624e62ea49fde
Mazliana <mazliana.mohamad@intel.com>
2018-12-13 14:12:03 +0800

    meta/lib/oeqa/selftest/cases/kernel_test1.py: changes on standard variable
    Signed-off-by: Mazliana <mazliana.mohamad@intel.com>

--- meta/lib/oeqa/selftest/cases/kernel_test1.py
+++ meta/lib/oeqa/selftest/cases/kernel_test1.py
@@ -6,21 +6,20 @@ from oeqa.utils.commands import bitbake, runqemu, get_bb_var, runCmd
 from oeqa.utils.git import GitRepo, GitError
 
 class KernelDevTc(OESelftestTestCase):
-    def test_gitadd(self):
+    def test_createpatch(self):
         writetoreadme = runCmd("echo This is a test to apply a patch to the kernel. >> tmp/work-shared/qemux86-64/kernel-source/README")
         self.assertEqual(writetoreadme.output, "")
         #This test step adds modified file "README" to git and creates a patch file "0001-KERNEL-DEV-TEST-CASE.patch" at the same location as file 
-        repo = GitRepo("tmp/work-shared/qemux86-64/kernel-source", is_topdir=True) 
-        os_path = os.path      
-        GIT_WORK_TREE = os.path.abspath('tmp/work-shared/qemux86-64/kernel-source/README')
-        repo.run_cmd('add %s' %(GIT_WORK_TREE))
+        repo = GitRepo("tmp/work-shared/qemux86-64/kernel-source", is_topdir=True)    
+        git_work_tree = os.path.abspath('tmp/work-shared/qemux86-64/kernel-source/README')
+        repo.run_cmd('add %s' %(git_work_tree))
         commit_message = "KERNEL DEV TEST CASE"
         git_cmd_commit = ['commit', '-m', commit_message]
         repo.run_cmd(git_cmd_commit) 
         git_cmd_patch = ['format-patch', '-1']
         repo.run_cmd(git_cmd_patch)
 
-    def test_createpatch(self):
+    def test_existingpatch(self):
         build_path = os.environ.get('BUILDDIR')
         poky_path, tail = os.path.split(build_path)
         poky_dir = os.chdir(poky_path)
@@ -32,7 +31,6 @@ class KernelDevTc(OESelftestTestCase):
         with open (file_name, 'w') as file:
             file.write("SRC_URI += \"file://0001-KERNEL-DEV-TEST-CASE.patch\"" + "\n")
             file.write('FILESEXTRAPATHS_prepend := \"${THISDIR}/${PN}:\"')
-        file.close() 
         readme_path = build_path + '/tmp/work-shared/qemux86-64/kernel-source/README'
         remove_readme_file = runCmd('rm %s ' %readme_path)
         self.assertEqual(remove_readme_file.output, "")  
