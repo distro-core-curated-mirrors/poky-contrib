4f01c34764f9ee9a8c0456989c6f49ba48a1bcb5
Mazliana <mazliana.mohamad@intel.com>
2019-01-09 14:33:28 +0800

    meta/lib/oeqa/selftest/cases/kernel_dev.py: Refactoring code Split function with setUp() and testcase no.1. Cleaning the code with naming variable
    Signed-off-by: Mazliana <mazliana.mohamad@intel.com>

--- meta/lib/oeqa/selftest/cases/kernel_dev.py
+++ meta/lib/oeqa/selftest/cases/kernel_dev.py
@@ -7,8 +7,9 @@ from oeqa.utils.commands import bitbake, runqemu, get_bb_var, runCmd
 from oeqa.utils.git import GitRepo, GitError
 
 class KernelDev(OESelftestTestCase):
-    
-    def setUpLocal(self):
+
+    def setUp(self):
+        #common prerequisites
         super(KernelDev, self).setUpLocal()
         self.recipe = 'core-image-minimal'
         self.machine = 'qemux86-64'
@@ -32,7 +33,7 @@ MACHINE = '%s'
         build_path = os.environ.get('BUILDDIR')
         poky_path, tail = os.path.split(build_path)
         poky_dir = os.chdir(poky_path)
-        layername = 'meta-kernelautomated'
+        layername = 'meta-kerneltest'
         layerpath = os.path.exists(layername)        
         self.assertTrue(os.path.exists(layerpath), '%s should not exist at this point in time' % layerpath)
         result = runCmd('bitbake-layers create-layer %s' %layername)
@@ -55,26 +56,26 @@ MACHINE = '%s'
         self.assertTrue(find_in_contents, "%s found in layers\n%s" % (layername, result.output))
     
     def test_apply_patches(self):
+        #tc01_kd_apply_patches
         result = runCmd('echo This is a test to apply a patch to the kernel. >> tmp/work-shared/qemux86-64/kernel-source/README')
-        self.assertEqual(result.output, '')
         #This test step adds modified file 'README' to git and creates a patch file '0001-KERNEL-DEV-TEST-CASE.patch' at the same location as file 
         repo = GitRepo('tmp/work-shared/qemux86-64/kernel-source', is_topdir=True)    
         git_work_tree = os.path.abspath('tmp/work-shared/qemux86-64/kernel-source/README')
         repo.run_cmd('add %s' %(git_work_tree))
         commit_message = 'KERNEL DEV TEST CASE'
-        git_cmd_commit = ['commit', '-m', commit_message]
-        repo.run_cmd(git_cmd_commit) 
-        git_cmd_patch = ['format-patch', '-1']
-        repo.run_cmd(git_cmd_patch)
+        git_commit = ['commit', '-m', commit_message]
+        repo.run_cmd(git_commit) 
+        git_patch = ['format-patch', '-1']
+        repo.run_cmd(git_patch)
         build_path = os.environ.get('BUILDDIR')
         poky_path, tail = os.path.split(build_path)
         poky_dir = os.chdir(poky_path)
         patch_file = build_path + '/tmp/work-shared/qemux86-64/kernel-source/0001-KERNEL-DEV-TEST-CASE.patch'
-        linux_yocto_path = poky_path + '/meta-kernelautomated/recipes-kernel/linux/linux-yocto/'
+        linux_yocto_path = poky_path + '/meta-kerneltest/recipes-kernel/linux/linux-yocto/'
         result = runCmd('mv %s %s' %(patch_file, linux_yocto_path))
-        self.assertEqual(result.output, '')
-        file_name = poky_path + '/meta-kernelautomated/recipes-kernel/linux/linux-yocto_4%.bbappend'
-        with open (file_name, 'w') as file:
+        self.assertFalse(os.path.exists(patch_file))
+        recipe_append = poky_path + '/meta-kerneltest/recipes-kernel/linux/linux-yocto_4%.bbappend'
+        with open (recipe_append, 'w') as file:
             file.write('SRC_URI += \'file://0001-KERNEL-DEV-TEST-CASE.patch\'' + '\n')
             file.write('FILESEXTRAPATHS_prepend := \'${THISDIR}/${PN}:\'')
         readme_path = build_path + '/tmp/work-shared/qemux86-64/kernel-source/README'
