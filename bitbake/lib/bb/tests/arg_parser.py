fedf0ffe7e0a2199ddcf831f7559976fe9668965
Chris Laplante <chris.laplante@agilent.com>
2020-11-08 17:10:52 -0500

    bitbake: tests/arg_parser: add test for 'bitbake -S none world'
    (Bitbake rev: 33afa36d1f783e7a99e5b10e5384292490bd6926)
    
    Signed-off-by: Chris Laplante <chris.laplante@agilent.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- bitbake/lib/bb/tests/arg_parser.py
+++ bitbake/lib/bb/tests/arg_parser.py
@@ -7,11 +7,37 @@
 # SPDX-License-Identifier: MIT
 #
 
+import shlex
 import unittest
+import collections
 
-from bb.main import create_bitbake_parser
+
+from bb.main import create_bitbake_parser, BitBakeConfigParameters
+
+ParseResult = collections.namedtuple("ParseResult", ("options", "targets"))
 
 
 class ArgParserTests(unittest.TestCase):
     def setUp(self):
         self._parser = create_bitbake_parser()
+
+    def _parse_helper(self, arg_str):
+        self.assertIs(type(arg_str), str)
+        params = BitBakeConfigParameters(argv=shlex.split(arg_str))
+        return ParseResult(params.options, params.options.pkgs_to_build)
+
+    def test_parse_s_none_world(self):
+        # All of these should give the same result
+        arg_strs = [
+            "bitbake -S none world",
+            "bitbake world -S none",
+            "bitbake -Snone world",
+            # optparse doesn't handle =
+            # "bitbake -S=none world",
+            # "bitbake world -S=none"
+        ]
+
+        for arg_str in arg_strs:
+            res = self._parse_helper(arg_str)
+            self.assertListEqual(res.targets, ["world"])
+            self.assertListEqual(res.options.dump_signatures, ["none"])
