b60943ac55c1923c301a2ff38d1625ec923c6519
Ross Burton <ross.burton@arm.com>
2021-05-17 12:21:22 +0100

    bb/utils: add layer_path() function
    Add layer_path() which will return the path to a layer. Useful to
    programatically find other layers at build time, or from tinfoil.
    
    Signed-off-by: Ross Burton <ross.burton@arm.com>

--- bitbake/lib/bb/utils.py
+++ bitbake/lib/bb/utils.py
@@ -1580,6 +1580,16 @@ def get_file_layer(filename, d, collection_res={}):
 
     return result
 
+def layer_path(layername, d):
+    """
+    Return the path to the specified layer, or None if the layer isn't present.
+    """
+    bbpath = d.getVar("BBPATH").split(":")
+    pattern = d.getVar('BBFILE_PATTERN_' + layername)
+    for path in reversed(sorted(bbpath)):
+        if re.match(pattern, path + "/"):
+            return path
+    return None
 
 # Constant taken from http://linux.die.net/include/linux/prctl.h
 PR_SET_PDEATHSIG = 1
