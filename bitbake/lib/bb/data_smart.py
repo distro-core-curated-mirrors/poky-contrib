bd7e8f8586e765643bbbdfa3c74387eaf259edeb
Richard Purdie <richard.purdie@linuxfoundation.org>
2021-07-30 14:14:49 +0100

    data store print
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- bitbake/lib/bb/data_smart.py
+++ bitbake/lib/bb/data_smart.py
@@ -480,6 +480,15 @@ class DataSmart(MutableMapping):
 
 
     def setVar(self, var, value, **loginfo):
+
+        if "_append" in var or "_prepend" in var or "_remove" in var:
+            info = "%s" % var
+            if "filename" in loginfo:
+                info += " file: %s" % loginfo[filename]
+            if "lineno" in loginfo:
+                info += " line: %s" % loginfo[lineno]
+            bb.fatal("Variable %s contains an operation using the old override syntax. Please convert this layer/metadata before attempting to use with a newer bitbake" % info)
+
         #print("var=" + str(var) + "  val=" + str(value))
 
         if "_append" in var or "_prepend" in var or "_remove" in var:
