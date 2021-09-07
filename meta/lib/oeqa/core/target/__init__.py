97d7785f1a7eb450422395ef8ab3fb74eb333ced
Ross Burton <ross.burton@arm.com>
2021-09-07 13:31:03 +0100

    oeqa/core/target: remove abstract decorations
    There's no need to inherit from object in Python 3, and marking the
    methods as abstract is overkill considering the type hierarchy.
    
    Signed-off-by: Ross Burton <ross.burton@arm.com>

--- meta/lib/oeqa/core/target/__init__.py
+++ meta/lib/oeqa/core/target/__init__.py
@@ -4,33 +4,26 @@
 # SPDX-License-Identifier: MIT
 #
 
-from abc import abstractmethod
 
-class OETarget(object):
+class OETarget:
 
     def __init__(self, logger, *args, **kwargs):
         self.logger = logger
 
-    @abstractmethod
     def start(self):
         pass
 
-    @abstractmethod
     def stop(self):
         pass
 
-    @abstractmethod
     def run(self, cmd, timeout=None):
         pass
 
-    @abstractmethod
     def copyTo(self, localSrc, remoteDst):
         pass
 
-    @abstractmethod
     def copyFrom(self, remoteSrc, localDst):
         pass
 
-    @abstractmethod
     def copyDirTo(self, localSrc, remoteDst):
         pass
