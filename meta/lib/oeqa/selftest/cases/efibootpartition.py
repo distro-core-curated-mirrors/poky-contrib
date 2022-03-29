17d6a1e08ca28e06e7942fe8a3103e60882a9498
Ross Burton <ross.burton@arm.com>
2022-03-29 15:48:32 +0100

    WIP efi refactor (should look like wic)

--- meta/lib/oeqa/selftest/cases/efibootpartition.py
+++ meta/lib/oeqa/selftest/cases/efibootpartition.py
@@ -5,42 +5,28 @@
 # SPDX-License-Identifier: MIT
 #
 
-import re
-
 from oeqa.selftest.case import OESelftestTestCase
-from oeqa.utils.commands import bitbake, runqemu, get_bb_var
+from oeqa.utils.commands import bitbake, runqemu
 
 class GenericEFITest(OESelftestTestCase):
     """EFI booting test class"""
-
-    cmd_common = "runqemu nographic serial wic ovmf"
-    efi_provider = "systemd-boot"
     image = "core-image-minimal"
-    machine = "qemux86-64"
-    recipes_built = False
 
-    @classmethod
-    def setUpLocal(self):
-        super(GenericEFITest, self).setUpLocal(self)
-
-        self.write_config(self,
-"""
-EFI_PROVIDER = "%s"
-IMAGE_FSTYPES:pn-%s:append = " wic"
-MACHINE = "%s"
-MACHINE_FEATURES:append = " efi"
-WKS_FILE = "efi-bootdisk.wks.in"
-IMAGE_INSTALL:append = " grub-efi systemd-boot kernel-image-bzimage"
-"""
-% (self.efi_provider, self.image, self.machine))
-        if not self.recipes_built:
-            bitbake("ovmf")
-            bitbake(self.image)
-            self.recipes_built = True
-
-    @classmethod
+    # TODO limit to x86
     def test_boot_efi(self):
         """Test generic boot partition with qemu"""
+        self.write_config(self, """
+EFI_PROVIDER = "systemd-boot"
+IMAGE_FSTYPES:pn-%s:append = " wic"
+MACHINE_FEATURES:append = " efi"
+WKS_FILE = "efi-bootdisk.wks.in"
+IMAGE_INSTALL:append = " grub-efi systemd-boot kernel-image"
+"""
+% (self.image))
+
+        bitbake("ovmf")
+        bitbake(self.image)
+
         cmd = "%s %s" % (self.cmd_common, self.machine)
-        with runqemu(self.image, ssh=False, launch_cmd=cmd) as qemu:
+        with runqemu(self.image, ssh=False, runqemuparams='nographic serial wic ovmf') as qemu:
             self.assertTrue(qemu.runner.logged, "Failed: %s" % cmd)
