14062a1c642227303bd0cf2749a38adba9353f89
Ross Burton <ross.burton@arm.com>
2022-10-31 19:46:17 +0000

    efibootpartition: improve test

--- meta/lib/oeqa/selftest/cases/efibootpartition.py
+++ meta/lib/oeqa/selftest/cases/efibootpartition.py
@@ -12,16 +12,11 @@ from oeqa.utils.commands import bitbake, runqemu
 
 class GenericEFITest(OESelftestTestCase):
     """EFI booting test class"""
-
-    cmd_common = "runqemu nographic serial wic ovmf"
-    efi_provider = "systemd-boot"
-    image = "core-image-minimal"
-    machine = "qemux86-64"
-    recipes_built = False
-
-    @classmethod
-    def setUpLocal(self):
-        super(GenericEFITest, self).setUpLocal(self)
+    def test_boot_efi(self):
+        cmd_common = "runqemu nographic serial wic ovmf"
+        efi_provider = "systemd-boot"
+        image = "core-image-minimal"
+        machine = "qemux86-64"
 
         self.write_config(self,
 """
@@ -33,14 +28,10 @@ WKS_FILE = "efi-bootdisk.wks.in"
 IMAGE_INSTALL:append = " grub-efi systemd-boot kernel-image-bzimage"
 """
 % (self.efi_provider, self.image, self.machine))
-        if not self.recipes_built:
-            bitbake("ovmf")
-            bitbake(self.image)
-            self.recipes_built = True
 
-    @classmethod
-    def test_boot_efi(self):
-        """Test generic boot partition with qemu"""
+        bitbake("ovmf")
+        bitbake(self.image)
+
         cmd = "%s %s" % (self.cmd_common, self.machine)
         with runqemu(self.image, ssh=False, launch_cmd=cmd) as qemu:
             self.assertTrue(qemu.runner.logged, "Failed: %s" % cmd)
