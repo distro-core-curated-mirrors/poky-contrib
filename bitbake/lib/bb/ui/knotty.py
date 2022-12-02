865dc070e4affbde07034acca6c3c4b9696b4d74
Joshua Watt <JPEWhacker@gmail.com>
2022-12-02 07:47:36 -0600

    knotty: Enable siggen tracking
    Signed-off-by: Joshua Watt <JPEWhacker@gmail.com>

--- bitbake/lib/bb/ui/knotty.py
+++ bitbake/lib/bb/ui/knotty.py
@@ -25,7 +25,7 @@ from itertools import groupby
 
 from bb.ui import uihelper
 
-featureSet = [bb.cooker.CookerFeatures.SEND_SANITYEVENTS, bb.cooker.CookerFeatures.BASEDATASTORE_TRACKING]
+featureSet = [bb.cooker.CookerFeatures.SEND_SANITYEVENTS, bb.cooker.CookerFeatures.BASEDATASTORE_TRACKING, bb.cooker.CookerFeatures.RECIPE_SIGGEN_INFO]
 
 logger = logging.getLogger("BitBake")
 interactive = sys.stdout.isatty()
