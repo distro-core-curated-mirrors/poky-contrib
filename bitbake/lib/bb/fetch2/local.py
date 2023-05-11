efd122629aaf8b0ffcb0b3130983130eed206660
Richard Purdie <richard.purdie@linuxfoundation.org>
2023-05-11 13:52:07 +0100

    Revert "bitbake: fetch2/local: Fix?"
    This reverts commit ca978ad2fef6f095bd55e53485382bba769307aa.

--- bitbake/lib/bb/fetch2/local.py
+++ bitbake/lib/bb/fetch2/local.py
@@ -41,7 +41,7 @@ class Local(FetchMethod):
         """
         Return the local filename of a given url assuming a successful fetch.
         """
-        return self.localfile_searchpaths(urldata, d)[1]
+        return self.localfile_searchpaths(urldata, d)[-1]
 
     def localfile_searchpaths(self, urldata, d):
         """
@@ -49,23 +49,17 @@ class Local(FetchMethod):
         """
         searched = []
         path = urldata.decodedurl
-        newpath = None
+        newpath = path
         if path[0] == "/":
             logger.debug2("Using absolute %s" % (path))
-            if os.path.exists(path):
-                return [path], path
-            else:
-                return [path], None
+            return [path]
         filespath = d.getVar('FILESPATH')
         if filespath:
             logger.debug2("Searching for %s in paths:\n    %s" % (path, "\n    ".join(filespath.split(":"))))
             newpath, hist = bb.utils.which(filespath, path, history=True)
             logger.debug2("Using %s for %s" % (newpath, path))
             searched.extend(hist)
-        # ensure newpath "" becomes None
-        if newpath:
-            return searched, newpath
-        return searched, None
+        return searched
 
     def need_update(self, ud, d):
         if ud.localpath and os.path.exists(ud.localpath):
