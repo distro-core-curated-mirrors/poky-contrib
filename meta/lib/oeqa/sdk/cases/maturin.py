395521b21afc2be8298ab16ca2c2ad3a75c28864
Tim Orling <tim.orling@konsulko.com>
2023-12-07 13:17:22 -0800

    [WIP] oeqa: add "maturin develop" SDK test case
    'maturin develop' first checks that a virtual environment
    has been created, which is a good test for our python3 SDK
    environment ;)
    
    Source for guessing-game lifted from https://www.maturin.rs/tutorial
    
    Currently fails to load 'core' crate.
    
    Needs at a minimum the following in e.g. local.conf:
    TOOLCHAIN_HOST_TASK:append = " nativesdk-python3-maturin"
    TOOLCHAIN_HOST_TASK:append = " packagegroup-rust-cross-canadian-${MACHINE}"
    
    Signed-off-by: Tim Orling <tim.orling@konsulko.com>

--- meta/lib/oeqa/sdk/cases/maturin.py
+++ meta/lib/oeqa/sdk/cases/maturin.py
@@ -4,9 +4,12 @@
 # SPDX-License-Identifier: MIT
 #
 
+import os
+import shutil
 import unittest
-from oeqa.sdk.case import OESDKTestCase
 
+from oeqa.core.utils.path import remove_safe
+from oeqa.sdk.case import OESDKTestCase
 from oeqa.utils.subprocesstweak import errors_have_output
 
 errors_have_output()
@@ -31,3 +34,41 @@ class MaturinTest(OESDKTestCase):
             output,
             r" - CPython %s (.+)/usr/bin/python%s$" % (python_version, python_version),
         )
+
+
+class MaturinDevelopTest(OESDKTestCase):
+    @classmethod
+    def setUpClass(self):
+        targetdir = os.path.join(self.tc.sdk_dir, "guessing-game")
+        try:
+            shutil.rmtree(targetdir)
+        except FileNotFoundError:
+            pass
+        shutil.copytree(
+            os.path.join(self.tc.files_dir, "maturin/guessing-game"), targetdir
+        )
+
+    def setUp(self):
+        machine = self.td.get("MACHINE")
+        if not (
+            self.tc.hasHostPackage("nativesdk-python3-maturin")
+            or self.tc.hasHostPackage("python3-maturin-native")
+        ):
+            raise unittest.SkipTest("No python3-maturin package in the SDK")
+        if not (
+            self.tc.hasHostPackage("packagegroup-rust-cross-canadian-%s" % machine)
+        ):
+            raise unittest.SkipTest(
+                "Testing 'maturin develop' requires Rust cross-canadian in the SDK"
+            )
+
+    def test_maturin_develop(self):
+        """
+        This test case requires:
+          (1) that a .venv has been created.
+          (2) a functional 'rustc'
+        """
+        self._run("cd %s/guessing-game; python3 -m venv .venv" % self.tc.sdk_dir)
+        cmd = "cd %s/guessing-game; maturin develop" % self.tc.sdk_dir
+        output = self._run(cmd)
+        self.assertEquals(output, "foo")
