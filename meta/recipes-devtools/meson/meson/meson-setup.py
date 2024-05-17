082680bed9cab69fc15ab8ee8301d6d76b6a0415
Ross Burton <ross.burton@arm.com>
2024-05-17 15:46:58 +0000

    meson: correct the host machine definition in SDKs
    The SDK_ARCH is not the correct definiton for the host machine definition
    inside the SDK, but because a nativesdk recipe doesn't know what the
    final target will be these values should be set up at SDK installation
    time via the environment script.
    
    Put placeholders in the installed meson.cross file instead, and replace
    them at SDK installation time with the correct values.
    
    Signed-off-by: Ross Burton <ross.burton@arm.com>

--- meta/recipes-devtools/meson/meson/meson-setup.py
+++ meta/recipes-devtools/meson/meson/meson-setup.py
@@ -1,8 +1,41 @@
 #!/usr/bin/env python3
 
 import os
+import re
 import string
 import sys
+
+# Keep these in sync with the logic in meson-routines.bbclass
+def meson_cpu_family():
+    arch = os.environ["OECORE_TARGET_ARCH"]
+    if arch == 'powerpc':
+        return 'ppc'
+    elif arch == 'powerpc64' or arch == 'powerpc64le':
+        return 'ppc64'
+    elif arch == 'armeb':
+        return 'arm'
+    elif arch == 'aarch64_be':
+        return 'aarch64'
+    elif arch == 'mipsel':
+        return 'mips'
+    elif arch == 'mips64el':
+        return 'mips64'
+    elif re.match(r"i[3-6]86", arch):
+        return "x86"
+    elif arch == "microblazeel":
+        return "microblaze"
+    else:
+        return arch
+
+def meson_operating_system():
+    opersys = os.environ["OECORE_TARGET_ARCH"]
+    if "mingw" in opersys:
+        return "windows"
+    # avoid e.g 'linux-gnueabi'
+    elif "linux" in opersys:
+        return "linux"
+    else:
+        return opersys
 
 class Template(string.Template):
     delimiter = "@"
@@ -29,6 +62,10 @@ template_file = os.path.join(sysroot, 'usr/share/meson/meson.cross.template')
 cross_file = os.path.join(sysroot, 'usr/share/meson/%smeson.cross' % os.environ["TARGET_PREFIX"])
 native_template_file = os.path.join(sysroot, 'usr/share/meson/meson.native.template')
 native_file = os.path.join(sysroot, 'usr/share/meson/meson.native')
+
+# Inject transformed values
+os.environ["OECORE_MESON_TARGET_FAMILY"] = meson_cpu_family()
+os.environ["OECORE_MESON_TARGET_OS"] = meson_operating_system()
 
 with open(template_file) as in_file:
     template = in_file.read()
