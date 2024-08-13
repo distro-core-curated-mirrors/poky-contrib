8c5dbac83e8aa905ce66d6e390a15c34b906d980
Richard Purdie <richard.purdie@linuxfoundation.org>
2024-08-13 15:08:13 +0100

    dwarfsrcfiles: Update to use modern license identification
    Switch to a SPDX-License-Identifier and rename files -> dwardsrcfiles.
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/recipes-devtools/dwarfsrcfiles/dwarfsrcfiles/dwarfsrcfiles.c
+++ meta/recipes-devtools/dwarfsrcfiles/dwarfsrcfiles/dwarfsrcfiles.c
@@ -3,9 +3,7 @@
 //
 // Copyright (C) 2011, Mark Wielaard <mjw@redhat.com>
 //
-// This file is free software.  You can redistribute it and/or modify
-// it under the terms of the GNU General Public License (GPL); either
-// version 2, or (at your option) any later version.
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include <argp.h>
 #include <stdio.h>
