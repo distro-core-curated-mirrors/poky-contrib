b1b6fdf4799b2f62e0dab819d059a1c07a52a1d7
Ross Burton <ross.burton@arm.com>
2024-10-10 15:16:33 +0100

    xserver-nodm-init: set runtime better

--- meta/recipes-graphics/x11-common/xserver-nodm-init/X11/Xsession.d/13xdgbasedirs.sh
+++ meta/recipes-graphics/x11-common/xserver-nodm-init/X11/Xsession.d/13xdgbasedirs.sh
@@ -1,13 +1,12 @@
 # Minimal/stub implementation of the XDG Base Directory specification.
 # http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
 #
-# Wayland needs XDG_RUNTIME_DIR, so set it to /tmp.  This isn't compliant with
-# the specification (wrong mode, wrong owner) but it's mostly sufficient.
-#
-# In the ideal case where SystemD is booting and userspace is initiated by a
-# SystemD user session this will have been set already, so don't overwrite it.
+# Wayland needs XDG_RUNTIME_DIR, so it hasn't been set already (by systemd,
+# elogind, pam) then create a directory in TMPDIR.
 
 if [ -z "$XDG_RUNTIME_DIR" ]; then
-    XDG_RUNTIME_DIR="/tmp"
+    XDG_RUNTIME_DIR=${TMPDIR:-/tmp}/runtime-${USER}
+    mkdir -p ${XDG_RUNTIME_DIR}
+    chmod 0700 ${XDG_RUNTIME_DIR}
     export XDG_RUNTIME_DIR
 fi
