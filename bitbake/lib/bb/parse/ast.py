84a29bdf07f5f93106c0a579cffb72a18226867b
Alexander Kanavin <alex@linutronix.de>
2024-11-14 12:10:47 +0100

    bitbake: parse: add support for 'addfragments' directive
    It takes two parameters:
    - location prefix for fragments
    - name of variable that holds the list of enabled fragments
    
    Implementation of this directive essentially expands the fragment list
    obtained from it into 'require' entries and hands them to the
    implementation of 'require'.
    
    (Bitbake rev: 4e374ab605140d6ce02f1d3277630f6d71a01e37)
    
    Signed-off-by: Alexander Kanavin <alex@linutronix.de>
    Signed-off-by: Mathieu Dubois-Briand <mathieu.dubois-briand@bootlin.com>

--- bitbake/lib/bb/parse/ast.py
+++ bitbake/lib/bb/parse/ast.py
@@ -326,6 +326,20 @@ class InheritDeferredNode(AstNode):
         inherits.append(self.inherit)
         data.setVar('__BBDEFINHERITS', inherits)
 
+class AddFragmentsNode(AstNode):
+    def __init__(self, filename, lineno, fragments_path_prefix, fragments_variable):
+        AstNode.__init__(self, filename, lineno)
+        self.path = fragments_path_prefix
+        self.variable = fragments_variable
+
+    def eval(self, data):
+        fragments = data.getVar(self.variable)
+        if not fragments:
+            return
+        for f in fragments.split():
+            full_fragment_name = data.expand("{}/{}.conf".format(self.path, f))
+            bb.parse.ConfHandler.include(self.filename, full_fragment_name, self.lineno, data, "include fragment")
+
 def handleInclude(statements, filename, lineno, m, force):
     statements.append(IncludeNode(filename, lineno, m.group(1), force))
 
@@ -369,6 +383,11 @@ def handleInherit(statements, filename, lineno, m):
 def handleInheritDeferred(statements, filename, lineno, m):
     classes = m.group(1)
     statements.append(InheritDeferredNode(filename, lineno, classes))
+
+def handleAddFragments(statements, filename, lineno, m):
+    fragments_path_prefix = m.group(1)
+    fragments_variable = m.group(2)
+    statements.append(AddFragmentsNode(filename, lineno, fragments_path_prefix, fragments_variable))
 
 def runAnonFuncs(d):
     code = []
